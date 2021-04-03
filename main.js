eb6ea442322d388547f6c74d290f13577dd9d83c
jjfire <81884559+jjfire@users.noreply.github.com>
2021-04-03 17:51:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("Project on Console");
+
+var n1="Jerush ";
+var n2="Jagani";
+
+var n3=n1.concat(n2);
+console.log(n3);
+
+var a=10;
+var b=5;
+
+
+var add= 10+5;
+console.log(add)
+
+var sub= 10-5;
+console.log(sub)
+
+var mul= 10*5;
+console.log(mul)
+
+var div= 10/5;
+console.log(div)
+
